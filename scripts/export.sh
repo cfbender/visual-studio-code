635ba5f89d9d729e460fd462d5e5f9278e817dcf
Cody Bender <codyfbender@gmail.com>
2019-10-29 15:51:17 -0600

    Update export script for clarity

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+# Up out of scripts
 cd ..
 npm install -yq
 npm run build
 
 VERSION=$(node -pe 'JSON.parse(process.argv[1]).version' "$(cat package.json)")
 
+# Up out of repo
 cd ..
-rsync -av --progress visual-studio-code/. $HOME/.vscode/extensions/dracula-italic-theme.theme-italic-dracula-$VERSION --exclude .git --exclude node_modules
+
+EXTENSIONS_DIR="${HOME}/.vscode/extensions/dracula-italic-theme.theme-italic-dracula-${VERSION}"
+
+rsync -av --progress visual-studio-code/. "$EXTENSIONS_DIR"  --exclude .git --exclude node_modules
 
